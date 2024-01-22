d63845daf4e16d0aaf435034e15da77034f0c0fb
EversonCordeiroSilva <everson.csilva99@gmail.com>
2024-01-22 20:33:43 -0300

    Auto-generated commit [ci skip]

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Olá! Sou Everson.';
+  desc = "Cristão, Solteiro, Desenvolvedor de softwares voltado para java mas que faz um pouco de tudo porque a tecnologia não para de evoluir e a inteligencia artificial será dominada!"
+  message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+}
